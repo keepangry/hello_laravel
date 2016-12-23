399e4417bb5f2aa72911ebcb35b7192c1a538a23
keepangry <0@keepangry.com>
2016-12-23 10:16:54 +0000

    1.1

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -8,9 +8,4 @@
       <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
     </form>
   @endcan
-  <form action="{{ route('users.destroy', $user->id) }}" method="post">
-    {{ csrf_field() }}
-    {{ method_field('DELETE') }}
-    <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
-  </form>
 </li>
